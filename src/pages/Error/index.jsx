0bad796b7346161a98e4e75448ac1a58d949f8ca
JohanHauteville <johan.hauteville@gmail.com>
2024-03-23 11:35:04 +0100

    Add redirect rule for netlify

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,13 @@
-
-import './styles.scss'
+import "./styles.scss";
+import image from "../../assets/img/404.svg";
 
 function Error() {
-    return<main className={"error-page"}>
-        <p>Erreur 404</p>
+  return (
+    <main className={"error-page"}>
+      <img className="error-image" src={image} alt="404" />
+      <p>Erreur 404</p>
     </main>
+  );
 }
 
 export default Error;
