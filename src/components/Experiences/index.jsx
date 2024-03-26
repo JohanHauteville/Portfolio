9c56e182c067ceec6ce2d95c382076547ac1ad44
JohanHauteville <johan.hauteville@gmail.com>
2024-03-26 17:49:21 +0100

    Update experiences data

--- src/components/Experiences/index.jsx
+++ src/components/Experiences/index.jsx
@@ -11,8 +11,14 @@ import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 import { Class, Work } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import data from "../../utils/data.json";
+
 function Experiences({ short = false }) {
   const navigate = useNavigate();
+  const experiences =
+    data.experiences && short === true
+      ? [...data.experiences.slice(0, 3)]
+      : [...data.experiences];
 
   function redirect() {
     navigate("/about#experiences");
@@ -30,94 +36,62 @@ function Experiences({ short = false }) {
             alt="experiences"
           />
           <Timeline className={"timeline"}>
-            <TimelineItem>
-              <TimelineOppositeContent
-                color="text.secondary"
-                sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
-              >
-                Février 2024 <br />
-                Décembre 2022
-                <br />
-                <br />
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot color={"kakiLight"}>
-                  <Class sx={{ color: "#fff" }} />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent
-                sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
-              >
-                <strong>
-                  Formation Développeur d'Application Frontend - React
-                </strong>
-                <br />
-                OpenClassrooms
-                <br />
-                <br />
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                color="text.secondary"
-                sx={{ fontSize: "calc(8px + 1vmin)" }}
-              >
-                2023
-                <br />
-                2018
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot color={"kaki"}>
-                  <Work sx={{ color: "#fff" }} />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ fontSize: "calc(10px + 1vmin)" }}>
-                <strong>
-                  Co-Fondateur:
-                  <br />
-                  Biscuiterie Kris et Jo
-                </strong>
-                <br />
-                Gestion de l'entreprise, création graphique, création et gestion
-                du e-commerce.
-                <br />
-                <br />
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                color="text.secondary"
-                sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
-              >
-                2017
-                <br />
-                2015
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot color={"kaki"}>
-                  <Work sx={{ color: "#fff" }} />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent
-                sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
-              >
-                <strong>
-                  Responsable logistique:
-                  <br />
-                  T.M.A.
-                </strong>
-                <br />
-                CDI, Montpellier
-                <br />
-                Chargé de communication, gestion du parc informatique,
-                responsable de la gestion des stocks.
-                <br />
-                <br />
-              </TimelineContent>
-            </TimelineItem>
+            {experiences &&
+              experiences.map((experience, index) => {
+                return (
+                  <TimelineItem key={"experiences-id-" + experience.id}>
+                    <TimelineOppositeContent
+                      color="text.secondary"
+                      sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
+                    >
+                      {experience.duration.map((date) => (
+                        <p
+                          className={"timeline__date"}
+                          key={"experiences-id-" + experience.id + "-" + date}
+                        >
+                          {date}
+                        </p>
+                      ))}
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                      {experience.type === "training" ? (
+                        <TimelineDot color={"kakiLight"}>
+                          <Class sx={{ color: "#fff" }} />
+                        </TimelineDot>
+                      ) : (
+                        <TimelineDot color={"kaki"}>
+                          <Work sx={{ color: "#fff" }} />
+                        </TimelineDot>
+                      )}
+
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent
+                      sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
+                    >
+                      <span className="timeline__job-name">
+                        {experience.jobName}
+                      </span>
+                      <br />
+                      {experience.typeOfContract && (
+                        <>
+                          <span className="timeline__contract-type">
+                            {experience.typeOfContract}
+                          </span>
+                          <br />
+                        </>
+                      )}
+
+                      <span className="timeline__content">
+                        {experience.content}
+                      </span>
+                      <br />
+                      <br />
+                    </TimelineContent>
+                  </TimelineItem>
+                );
+              })}
+
             {short === true ? (
               <>
                 <span className={"experiences-dot"}>
@@ -141,217 +115,7 @@ function Experiences({ short = false }) {
                 </Button>
               </>
             ) : (
-              <>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingRight: "0" }}
-                  >
-                    2015
-                    <br />
-                    2014
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kaki"}>
-                      <Work sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    <strong>
-                      Graphiste maquétiste VFX: <br />
-                      Freelance
-                    </strong>
-                    <br />
-                    Réalisation de plaquettes, de cartes de visite et de vidéos
-                    marketing.
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    2014
-                    <br />
-                    2012
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kaki"}>
-                      <Work sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
-                  >
-                    <strong>
-                      Animateur 3D / Graphiste 2D
-                      <br />
-                      IP. Studio
-                    </strong>
-                    <br />
-                    Co-Fondateur, Montpellier
-                    <br />
-                    Responsable du pôle création des animations 3D d'un jeu
-                    mobile.
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingRight: "0" }}
-                  >
-                    2012
-                    <br />
-                    2009
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kakiLight"}>
-                      <Class sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    <strong>
-                      Formation: <br />
-                      Graphiste et Animation 3D
-                    </strong>
-                    <br />
-                    Aries, Pérols
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    2009
-                    <br />
-                    2008
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kaki"}>
-                      <Work sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
-                  >
-                    <strong>
-                      Stage
-                      <br />
-                      Réparateur Informatique
-                    </strong>
-                    <br />
-                    FOCS, Jacou
-                    <br />
-                    Maintenance des serveurs informatiques, réparation
-                    d'ordinateurs Hardware pour les professionnels.
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingRight: "0" }}
-                  >
-                    2008
-                    <br />
-                    2007
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kaki"}>
-                      <Work sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    <strong>
-                      Stage
-                      <br />
-                      Responsable Informatique
-                    </strong>
-                    <br />
-                    Mairie de Bouzigues
-                    <br />
-                    Maintenance des serveurs informatiques, réparation
-                    d'ordinateurs Hardware pour les professionnels.
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    2009
-                    <br />
-                    2006
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kakiLight"}>
-                      <Class sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingRight: "0" }}
-                  >
-                    <strong>
-                      Brevet de Technicien Supérieur
-                      <br />
-                      Informatique et Réseaux
-                    </strong>
-                    <br />
-                    Lycée Jean Mermoz, Montpellier
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                  <TimelineOppositeContent
-                    color="text.secondary"
-                    sx={{ fontSize: "calc(8px + 1vmin)", paddingRight: "0" }}
-                  >
-                    2006
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot color={"kakiLight"}>
-                      <Class sx={{ color: "#fff" }} />
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent
-                    sx={{ fontSize: "calc(10px + 1vmin)", paddingLeft: "0" }}
-                  >
-                    <strong>
-                      Baccalauréat
-                      <br />
-                      Sciences et Technologies Industrielles génie Électronique
-                    </strong>
-                    <br />
-                    Lycée Joliot Curie, Sète
-                    <br />
-                    <br />
-                  </TimelineContent>
-                </TimelineItem>
-              </>
+              <></>
             )}
           </Timeline>
         </div>
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,5 +1,5 @@
 import "./styles.scss";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   Button,
   Card,
